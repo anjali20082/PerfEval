eea559bd215bcf8203fd338186ae39c20326cbbd
Anjali Jain <jainanjali13224@gmail.com>
2021-09-27 22:17:59 +0530

    Added code for automation in field experiments

---
+++ src/main/java/iiitd/nrl/evalapp/PingUpload.java
@@ -0,0 +1,18 @@
+package iiitd.nrl.evalapp;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+    public class PingUpload {
+
+        @BeforeMethod
+        public void launchCap() {
+            MyDatabase.sendPINGLog();
+        }
+
+        @Test
+        public void dummyTest() throws InterruptedException {
+            System.out.println("Uploading Ping Data :)");
+        }
+    }
+
