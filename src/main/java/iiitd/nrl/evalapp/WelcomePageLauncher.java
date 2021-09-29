7c07bfa7f889c477761f529e97835f4312fb67e9
Shradha Sabhlok <shradha20069@iiitd.ac.in>
2021-09-29 18:18:18 +0530

    Amazon Fixed.

--- src/main/java/iiitd/nrl/evalapp/WelcomePageLauncher.java
+++ src/main/java/iiitd/nrl/evalapp/WelcomePageLauncher.java
@@ -96,77 +96,77 @@ public class WelcomePageLauncher extends Application {
             addTestsToSuite(suiteFiles, tb_classes, "Yts");
 
 
-//            if (youtube_cb.isSelected()) {
-//                List<XmlClass> yts_classes = new ArrayList<XmlClass>();
-//                yts_classes.add(new XmlClass("iiitd.nrl.evalapp.YouTubeTests_SearchChannel"));
-//                yts_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, yts_classes, "Yts");
-//
-//                List<XmlClass> ytp_classes = new ArrayList<XmlClass>();
-//                ytp_classes.add(new XmlClass("iiitd.nrl.evalapp.YouTubeTests_PlayVideo"));
-//                ytp_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, ytp_classes, "Ytp");
-//
-//
-//                MyDatabase.totalTests += 2;
-//            }
-//
-//            if (hotstar_cb.isSelected()) {
-//                List<XmlClass> hs_classes = new ArrayList<XmlClass>();
-//                hs_classes.add(new XmlClass("iiitd.nrl.evalapp.HotstarTests_search"));
-//                hs_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, hs_classes, "Hs");
-//
-//
-//                List<XmlClass> classes = new ArrayList<XmlClass>();
-//                classes.add(new XmlClass("iiitd.nrl.evalapp.HotstarTests_trending"));
-//                classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, classes, "Ht");
-//
-//
-//                MyDatabase.totalTests += 2;
-//            }
-//
-//            if (linkedin_cb.isSelected()) {
-//                List<XmlClass> lp_classes = new ArrayList<XmlClass>();
-//                lp_classes.add(new XmlClass("iiitd.nrl.evalapp.LinkedInTests_profile"));
-//                lp_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, lp_classes, "Lp");
-//
-//                List<XmlClass> lc_classes = new ArrayList<XmlClass>();
-//                lc_classes.add(new XmlClass("iiitd.nrl.evalapp.LinkedInTests_myConnections"));
-//                lc_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, lc_classes, "Lc");
-//
-//                List<XmlClass> ls_classes = new ArrayList<XmlClass>();
-//                ls_classes.add(new XmlClass("iiitd.nrl.evalapp.LinkedInTests_search"));
-//                ls_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, ls_classes, "Ls");
-//
-//                MyDatabase.totalTests += 3;
-//            }
-//
-//            if (facebook_cb.isSelected()) {
-//                List<XmlClass> fp_classes = new ArrayList<XmlClass>();
-//                fp_classes.add(new XmlClass("iiitd.nrl.evalapp.FacebookTests_post"));
-//                fp_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, fp_classes, "Fp");
-//
-//                List<XmlClass> fs_classes = new ArrayList<XmlClass>();
-//                fs_classes.add(new XmlClass("iiitd.nrl.evalapp.FacebookTests_search"));
-//                fs_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, fs_classes, "Fs");
-//
-//                MyDatabase.totalTests += 2;
-//            }
-//
-//            if (flipkart_cb.isSelected()) {
-//                List<XmlClass> classes = new ArrayList<XmlClass>();
-//                classes.add(new XmlClass("iiitd.nrl.evalapp.FlipkartTests"));
-//                classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
-//                addTestsToSuite(suiteFiles, classes, "Fl");
-//                MyDatabase.totalTests += 1;
-//            }
+            if (youtube_cb.isSelected()) {
+                List<XmlClass> yts_classes = new ArrayList<XmlClass>();
+                yts_classes.add(new XmlClass("iiitd.nrl.evalapp.YouTubeTests_SearchChannel"));
+                yts_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, yts_classes, "Yts");
+
+                List<XmlClass> ytp_classes = new ArrayList<XmlClass>();
+                ytp_classes.add(new XmlClass("iiitd.nrl.evalapp.YouTubeTests_PlayVideo"));
+                ytp_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, ytp_classes, "Ytp");
+
+
+                MyDatabase.totalTests += 2;
+            }
+
+            if (hotstar_cb.isSelected()) {
+                List<XmlClass> hs_classes = new ArrayList<XmlClass>();
+                hs_classes.add(new XmlClass("iiitd.nrl.evalapp.HotstarTests_search"));
+                hs_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, hs_classes, "Hs");
+
+
+                List<XmlClass> classes = new ArrayList<XmlClass>();
+                classes.add(new XmlClass("iiitd.nrl.evalapp.HotstarTests_trending"));
+                classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, classes, "Ht");
+
+
+                MyDatabase.totalTests += 2;
+            }
+
+            if (linkedin_cb.isSelected()) {
+                List<XmlClass> lp_classes = new ArrayList<XmlClass>();
+                lp_classes.add(new XmlClass("iiitd.nrl.evalapp.LinkedInTests_profile"));
+                lp_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, lp_classes, "Lp");
+
+                List<XmlClass> lc_classes = new ArrayList<XmlClass>();
+                lc_classes.add(new XmlClass("iiitd.nrl.evalapp.LinkedInTests_myConnections"));
+                lc_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, lc_classes, "Lc");
+
+                List<XmlClass> ls_classes = new ArrayList<XmlClass>();
+                ls_classes.add(new XmlClass("iiitd.nrl.evalapp.LinkedInTests_search"));
+                ls_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, ls_classes, "Ls");
+
+                MyDatabase.totalTests += 3;
+            }
+
+            if (facebook_cb.isSelected()) {
+                List<XmlClass> fp_classes = new ArrayList<XmlClass>();
+                fp_classes.add(new XmlClass("iiitd.nrl.evalapp.FacebookTests_post"));
+                fp_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, fp_classes, "Fp");
+
+                List<XmlClass> fs_classes = new ArrayList<XmlClass>();
+                fs_classes.add(new XmlClass("iiitd.nrl.evalapp.FacebookTests_search"));
+                fs_classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, fs_classes, "Fs");
+
+                MyDatabase.totalTests += 2;
+            }
+
+            if (flipkart_cb.isSelected()) {
+                List<XmlClass> classes = new ArrayList<XmlClass>();
+                classes.add(new XmlClass("iiitd.nrl.evalapp.FlipkartTests"));
+                classes.add(new XmlClass("iiitd.nrl.evalapp.TrakBytesUpload"));
+                addTestsToSuite(suiteFiles, classes, "Fl");
+                MyDatabase.totalTests += 1;
+            }
 
             if (amazon_cb.isSelected()) {
                 List<XmlClass> classes = new ArrayList<XmlClass>();
